c82a45c627102f6d2a2a5af69ff4021d08687ba9
vini-coelho <vinicius_coelho@live.com>
2019-06-09 17:30:11 -0400

    water waves design

--- src/components/Water.js
+++ src/components/Water.js
@@ -1,13 +1,71 @@
 import React from 'react';
 import { View, StyleSheet, Dimensions } from 'react-native';
 
-export default props => 
-    <View style={[styles.container, {height: props.alt}]}>
+const windowWidth = Dimensions.get('window').width
 
+export default props => 
+    <View>
+        <View style={styles.bubbleContainer3}>
+            <View style={[styles.bubble2, {backgroundColor: '#11aaff'}]}></View>
+            <View style={[styles.bubble2, {backgroundColor: '#11aaff'}]}></View>
+            <View style={[styles.bubble2, {backgroundColor: '#11aaff'}]}></View>
+        </View>
+        <View style={styles.bubbleContainer2}>
+        <View style={[styles.bubble, {backgroundColor: '#11ccff'}]}></View>
+            <View style={[styles.bubble, {backgroundColor: '#11ccff'}]}></View>
+            <View style={[styles.bubble, {backgroundColor: '#11ccff'}]}></View>
+            <View style={[styles.bubble, {backgroundColor: '#11ccff'}]}></View>
+            <View style={[styles.bubble, {backgroundColor: '#11ccff'}]}></View>
+        </View>
+        <View style={styles.bubbleContainer}>
+            <View style={styles.bubble}></View>
+            <View style={styles.bubble}></View>
+            <View style={styles.bubble}></View>
+            <View style={styles.bubble}></View>
+        </View>
+        
+        <View style={[styles.container, {height: props.alt}]}>
+        </View>
     </View>
 
 const styles = StyleSheet.create({
+    bubble: {
+        top: 0,
+        height: windowWidth/4,
+        width: windowWidth/4,
+        borderRadius: windowWidth/8,
+        backgroundColor: '#1199ff'
+    },
+    bubble2: {
+        top: 0,
+        height: windowWidth/3,
+        width: windowWidth/3,
+        borderRadius: windowWidth/6,
+        backgroundColor: '#1199ff'
+    },
     container: {
         backgroundColor: '#1199ff',
     },
+    bubbleContainer: {
+        position: 'absolute',
+        top: windowWidth/3.5,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        flexDirection: 'row',
+    },  
+    bubbleContainer2: {
+        position: 'absolute',
+        top: windowWidth/3.3,
+        left: windowWidth/8,
+        right: 0,
+        bottom: 0,
+        flexDirection: 'row',
+    },  
+    bubbleContainer3: {
+        right: 0,
+        bottom: 0,
+        top: windowWidth/3.6,
+        flexDirection: 'row',
+    }, 
 })
--- src/screens/AmountSelection.js
+++ src/screens/AmountSelection.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { Dimensions, Text, View, StyleSheet, Modal, ScrollView, TouchableOpacity } from 'react-native';
 
 const items = [
-    { value: 100 },{ value: 200 },{ value: 300 },
-    { value: 400 },{ value: 500 },{ value: 600 },
-    { value: 700 },{ value: 800 },{ value: 900 },
-    { value: 1000 },{ value: 1100 },{ value: 1200 },
-    { value: 1300 },{ value: 1400 },{ value: 1500 },
+    { value: 100 },{ value: 150 },{ value: 200 },
+    { value: 250 },{ value: 300 },{ value: 350 },
+    { value: 400 },{ value: 450 },{ value: 500 },
+    { value: 550 },{ value: 600 },{ value: 650 },
+    { value: 700 },{ value: 750 },{ value: 800 },{ value: 850 },
+    { value: 900 },{ value: 950 },{ value: 1000 },
+    { value: 1100 },{ value: 1150 },{ value: 1200 },
+    { value: 1250 },{ value: 1300 },{ value: 1350 },
+    { value: 1400 },{ value: 1450 },{ value: 1500 },
 ]
 
 export default props => 
